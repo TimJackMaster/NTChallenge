2adc6b6bc69bdab2c0cb70a6f0c477cf9fb14209
timba <timjackmaster@gmail.com>
2022-09-13 18:05:05 +0200

    introduce application layer with simple service that returns mock answer

---
+++ Application/Core/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Core
+{
+    public static class ServiceExtensions
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IWeatherInformationService, WeatherInformationService>();
+        }
+    }
+}
---
+++ Application/Services/WeatherInformationService.cs
@@ -0,0 +1,11 @@
+﻿using Domain;
+
+namespace Application.Services;
+
+internal sealed class WeatherInformationService : IWeatherInformationService
+{
+    public Task<WeatherInformation> GetCurrentWeatherInformation()
+    {
+        return Task.FromResult(new WeatherInformation(1.1m));
+    }
+}
